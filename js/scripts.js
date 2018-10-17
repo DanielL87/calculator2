9e0e1706db3047cc8665ac21825426e0fbd9b8b9
Daniel Lira and Gulzat Karimova <daniel@>
2018-10-17 11:47:33 -0700

    Added subtruct, multiply, devide

--- js/scripts.js
+++ js/scripts.js
@@ -29,4 +29,28 @@ $(document).ready(function() {
     $("#output").text(result)
   });
 
+    $("form#subtract").submit(function(event) {
+      event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result2 = subtract(number1, number2);
+    $("#output").text(result2)
+    });
+
+    $("form#multiply").submit(function(event) {
+      event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result3 = multiply(number1, number2);
+    $("#output").text(result3)
+    });
+
+    $("form#devide").submit(function(event) {
+      event.preventDefault();
+    var number1 = parseInt($("#devide1").val());
+    var number2 = parseInt($("#devide2").val());
+    var result4 = devide(number1, number2);
+    $("#output").text(result4)
+
+  });
 });
